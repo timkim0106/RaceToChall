e735612283adfecb62eac7332806cb766edd3d33
Tae Ho Kim <tkim12@uci.edu>
2025-01-26 03:08:24 -0800

    last frontend changes again

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,2 +1,2 @@
 import cassiopeia as cass
-cass.set_riot_api_key("RGAPI-d7e6ead7-a997-4734-9033-69bb09a5935b")  # This overrides the value set in your configuration/settings.
+cass.set_riot_api_key("")  # This overrides the value set in your configuration/settings.
